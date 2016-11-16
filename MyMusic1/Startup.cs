0a44fa0e25678a3c5f7e1a35182e04be9fdd90c6
HienD <HienD@DESKTOP-763JQE6>
2016-11-16 12:38:48 -0800

    Add project files.

---
+++ MyMusic1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMusic1.Startup))]
+namespace MyMusic1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
